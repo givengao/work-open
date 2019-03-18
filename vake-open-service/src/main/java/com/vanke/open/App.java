723d9a524eeda5dec44fdad9788acaced66a3403
c-gaozh <c-gaozh@vanke.cn>
2019-03-18 13:37:57 +0800

    1.init

---
+++ vake-open-service/src/main/java/com/vanke/open/App.java
@@ -0,0 +1,13 @@
+package com.vanke.open;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
